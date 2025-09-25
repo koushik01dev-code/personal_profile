dcb82933e824998fd5770be6bde1c97021acc8ea
koushik18 <koushik@altdigital.tech>
2025-09-25 18:08:04 +0530

    innitial commit

---
+++ src/main/java/com/profile/koushik_profile/KoushikProfileApplication.java
@@ -0,0 +1,13 @@
+package com.profile.koushik_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoushikProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoushikProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profile/koushik_profile/KoushikProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profile.koushik_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoushikProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
